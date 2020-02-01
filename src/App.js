72af31bc965c974116d7e0212551f3db7215a8f3
Blari <Blarior@gmail.com>
2020-02-01 20:37:46 +0300

    Add home page

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from "react";
 import "./App.css";
+import HomePage from "./homepage.component";
 
 function App() {
-  return;
-  <div>Hello world</div>;
+  return (
+    <div>
+      <HomePage />
+    </div>
+  );
 }
-
 export default App;
---
+++ src/homepage.component.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+const HomePage = () => (
+  <div className="homapage">
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">HATS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">JACKETS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">SNEAKERS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">WOMANS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+    <div className="directory-menu">
+      <div className="menu-item">
+        <div className="content">
+          <h1 className="title">MANS</h1>
+          <span className="subtitle">SHOP NOW</span>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default HomePage;
