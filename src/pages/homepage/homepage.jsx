3292128d18bc08230340a57cd8fc82f0eec8ba0e
Blari <Blarior@gmail.com>
2020-02-03 00:55:19 +0300

    Add more components

---
+++ src/pages/homepage/homepage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./homepage.styles.scss";
+
+import Directory from "../../components/derectory/directory";
+
+const HomePage = () => (
+  <div className="homepage">
+    <Directory />
+  </div>
+);
+
+export default HomePage;
