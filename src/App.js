d3bf206db0964048ea058a99f09ef50e426bbbd8
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-02-04 10:33:00 +0300

    Update menu to navigate withRoute HOC

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import React from "react";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage";
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+
+const HatsPage = () => (
+  <div>
+    <h1>Hats Page</h1>
+  </div>
+)
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={HomePage} />
+          <Route path='/hats' components={HatsPage} />
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
