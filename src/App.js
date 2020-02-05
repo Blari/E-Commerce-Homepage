0504e8eda8175d39347f73c53fb9f7966593fc1e
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-02-05 10:52:26 +0300

    updating our app with a header

--- src/App.js
+++ src/App.js
@@ -5,11 +5,13 @@ import "./App.css";
 
 import HomePage from "./pages/homepage/homepage";
 import ShopPage from './pages/shop/shop';
+import Header from './components/header/header'
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
