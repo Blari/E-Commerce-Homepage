f02a43e8caadb113373dee0f99650f17b5dc54b2
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-02-12 17:53:58 +0300

    Updates

---
+++ precache-manifest.c7f0bf0657917f8fd0aa8b23b9587abd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0ef8a161e99523a82ee93fe452bbeb0",
+    "url": "/E-Commerce-Homepage/index.html"
+  },
+  {
+    "revision": "5572707f00917656c137",
+    "url": "/E-Commerce-Homepage/static/css/main.3a5f5ae7.chunk.css"
+  },
+  {
+    "revision": "6d45964a2a300b26d0da",
+    "url": "/E-Commerce-Homepage/static/js/2.25fd396b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/E-Commerce-Homepage/static/js/2.25fd396b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5572707f00917656c137",
+    "url": "/E-Commerce-Homepage/static/js/main.f50378d2.chunk.js"
+  },
+  {
+    "revision": "169b5cc4a420de503074",
+    "url": "/E-Commerce-Homepage/static/js/runtime-main.98b6c4ff.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/E-Commerce-Homepage/static/media/crown.ed28accb.svg"
+  }
+]);
