42df9e453830b437446f97e91aa14781d7b622b0
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-02-12 18:16:59 +0300

    Some code ref

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route exact path='/signin' component={SignInAndSignUpPage} />
+          <Route path='/signin' component={SignInAndSignUpPage} />
         </Switch>
       </BrowserRouter>
     </div>
