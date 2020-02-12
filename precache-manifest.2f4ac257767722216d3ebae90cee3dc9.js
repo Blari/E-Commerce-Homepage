5069af69740ff9ce2b223fe669a69319ee022bd2
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-02-12 11:05:40 +0300

    Updates

---
+++ precache-manifest.2f4ac257767722216d3ebae90cee3dc9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55ec8a9cda2e900f2da8f07314f522b2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2d2f3b8f6fee837b35f2",
+    "url": "./static/css/main.95930347.chunk.css"
+  },
+  {
+    "revision": "da893b9b22ba395c79fa",
+    "url": "./static/js/2.ba96a982.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.ba96a982.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d2f3b8f6fee837b35f2",
+    "url": "./static/js/main.e0da2519.chunk.js"
+  },
+  {
+    "revision": "e72bd215622bc565b1f5",
+    "url": "./static/js/runtime-main.bb865f24.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "./static/media/crown.ed28accb.svg"
+  }
+]);
