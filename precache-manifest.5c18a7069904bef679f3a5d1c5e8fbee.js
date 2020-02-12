4db536235fdb2a597e4acddedae3a0056b1640b8
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-02-12 18:14:03 +0300

    Updates

---
+++ precache-manifest.5c18a7069904bef679f3a5d1c5e8fbee.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a4be8479f2018fe1db0e05ab5f30fb1a",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1ed84c6f9b43c019437d",
+    "url": "./static/css/main.3a5f5ae7.chunk.css"
+  },
+  {
+    "revision": "6d45964a2a300b26d0da",
+    "url": "./static/js/2.25fd396b.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "./static/js/2.25fd396b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1ed84c6f9b43c019437d",
+    "url": "./static/js/main.27143759.chunk.js"
+  },
+  {
+    "revision": "e72bd215622bc565b1f5",
+    "url": "./static/js/runtime-main.bb865f24.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "./static/media/crown.ed28accb.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/E-Commerce-Homepage/precache-manifest.89fad7194c0c7b0669bb33ac48b56f90.js"
+  "./precache-manifest.5c18a7069904bef679f3a5d1c5e8fbee.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/E-Commerce-Homepage/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
