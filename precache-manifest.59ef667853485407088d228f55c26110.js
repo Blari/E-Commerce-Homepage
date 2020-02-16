7d9774138c66b56c11c92e66052bbed68bd040b9
Blari <Blarior@gmail.com>
2020-02-16 11:16:30 +0300

    Updates

---
+++ precache-manifest.59ef667853485407088d228f55c26110.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4af1ab9c36abcbb5ade2907133ca7c44",
+    "url": "/Blari/E-Commerce-Homepage/index.html"
+  },
+  {
+    "revision": "8e990c533cf63f123515",
+    "url": "/Blari/E-Commerce-Homepage/static/css/main.c7c310e4.chunk.css"
+  },
+  {
+    "revision": "a07e22047fe3d1130b8f",
+    "url": "/Blari/E-Commerce-Homepage/static/js/2.9640d95b.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/Blari/E-Commerce-Homepage/static/js/2.9640d95b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e990c533cf63f123515",
+    "url": "/Blari/E-Commerce-Homepage/static/js/main.f68fdcab.chunk.js"
+  },
+  {
+    "revision": "dc661e4ca0583f268814",
+    "url": "/Blari/E-Commerce-Homepage/static/js/runtime-main.47ab3266.js"
+  },
+  {
+    "revision": "19ef7b17bc9414c63c7781251ce0d24d",
+    "url": "/Blari/E-Commerce-Homepage/static/media/crown.19ef7b17.svg"
+  }
+]);
