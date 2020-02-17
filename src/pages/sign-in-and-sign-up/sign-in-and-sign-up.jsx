f68ff3976fe17ca6f4692ac99a00d72534b11fc4
Alexsandr Rubanau <a.rubanau@flatlogic.com>
2020-02-17 10:26:38 +0300

    implemented firebase utils

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
 import './sign-in-and-sign-up.scss';
-import SingIn from '../../components/sign-in/sign-in'
+import SingIn from '../../components/sign-in/sign-in';
+import SignUp from '../../components/sign-up/sing-up';
+
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <SingIn />
+        <SignUp />
     </div>
 );
 
